105b3a225aa9e5ba715d1c762556547aee36844a
Matthew Levine <mattlevine22@gmail.com>
2020-09-21 11:54:12 -0700

    more thorough loop over sigma vs gamma

--- Experiments/Lorenz3D/Local/ARNN_test.sh
+++ Experiments/Lorenz3D/Local/ARNN_test.sh
@@ -10,11 +10,12 @@ for HID in ARNN
 do
 for OUT in simpleRHS
 do
-for SIG in 0.75 1 2 5
+for SIG in 0.1 0.75 1 2 5 10
 do
-for GAM in 15 20 25 30
+for GAM in 0 1 10 15 25 50
 do
 python3 RUN.py esn \
+--dont_redo 1 \
 --use_tilde $TIL \
 --hidden_dynamics $HID \
 --output_dynamics $OUT \
@@ -22,7 +23,7 @@ python3 RUN.py esn \
 --lambda 0 \
 --sigma_input $SIG \
 --regularization 0.0000001 \
---dynamics_length 2000 \
+--dynamics_length 1000 \
 --iterative_prediction_length 1000 \
 --solver auto \
 --noise_level 0 \
@@ -34,7 +35,7 @@ python3 RUN.py esn \
 --mode all \
 --display_output 1 \
 --scaler standard \
---approx_reservoir_size 3000 \
+--approx_reservoir_size 2000 \
 --degree 10 \
 --radius 0.6 \
 --num_test_ICS 1 \
