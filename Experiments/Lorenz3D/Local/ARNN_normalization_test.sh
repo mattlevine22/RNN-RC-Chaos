11a4615aeec21a21f54766e56bb65a5da9a89599
Matthew Levine <mattlevine22@gmail.com>
2020-09-23 14:23:14 -0700

    add L63 fully continuous RC test scripts for RDIM=3

---
+++ Experiments/Lorenz3D/Local/ARNN_normalization_test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cd ../../../Methods
+
+for SCALE in no standard
+do
+python3 RUN.py esn \
+--use_tilde 1 \
+--hidden_dynamics ARNN \
+--output_dynamics simpleRHS \
+--gamma 20 \
+--mode all \
+--display_output 1 \
+--system_name Lorenz3D \
+--write_to_log 1 \
+--N 100000 \
+--N_used 5000 \
+--RDIM 3 \
+--noise_level 0 \
+--scaler $SCALE \
+--approx_reservoir_size 2000 \
+--degree 10 \
+--radius 0.5 \
+--sigma_input 1 \
+--regularization 0.000001 \
+--dynamics_length 1000 \
+--iterative_prediction_length 2000 \
+--num_test_ICS 3 \
+--solver auto \
+--number_of_epochs 1000000 \
+--learning_rate 0.001 \
+--reference_train_time 10 \
+--buffer_train_time 0.5
+done
